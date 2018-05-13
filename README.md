cb9fa5935fa675511ca8d5d8e2ac3082eb920979
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-05-13 08:41:59 -0700

    Adds basic ncurses UI

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ PSU CS557 final project.
 3. Go to the repository folder in your terminal
 4. `stack init`
 5. `stack build`
-6. `stack exec` (to run the program)
+6. `stack exec todo-hs-exe` (run the program, make sure to build after you've made changes)
 
 ## Code Layout/Architecture
 
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,35 @@
 module Main where
 
 import Lib
+import UI.NCurses
 
 main :: IO ()
-main = someFunc
+main = runCurses $ do
+    setEcho False
+    w <- defaultWindow
+    updateWindow w $ do
+        moveCursor 1 10
+        drawString "Best To Do App Ever!"
+        moveCursor 2 10
+        drawString "===================="
+        moveCursor 4 1
+        drawString "1. List events"
+        moveCursor 5 1
+        drawString "2. List deadline items"
+        moveCursor 6 1
+        drawString "3. Enter new event"
+        moveCursor 7 1
+        drawString "4. Enter new deadline item"
+        moveCursor 10 10
+        drawString "(press q to quit)"
+        moveCursor 0 0
+    render
+    waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
+
+waitFor :: Window -> (Event -> Bool) -> Curses ()
+waitFor w p = loop where
+    loop = do
+        ev <- getEvent w Nothing
+        case ev of
+            Nothing -> loop
+            Just ev' -> if p ev' then return () else loop
