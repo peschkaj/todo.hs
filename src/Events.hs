4a784d0797a1f16a2882f3208c095e7134551b6c
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2018-06-05 21:23:51 -0700

    Switches addEvent to using Either (#13)
    - creates an `addEventToList` that returns an `Either [Event] [Event]`
    - Modifies `addEvent` to use that `Either`

--- src/Events.hs
+++ src/Events.hs
@@ -64,15 +64,18 @@ getCurrentEvents = decodeFileStrict eventFile >>= (\x -> case x of
                         Nothing   -> return [])
 
 
--- write the new Event to the Events file if it does not overlap with existing event, otherwise 
--- do not add and display error message
-addEvent :: Event -> [Event] -> IO ()
-addEvent x es = do encodeFile eventFile (snd result)
-                   if (fst result) then
-                      putStrLn ("\nEvent added to Schedule successfully.\n")
-                   else
-                      putStrLn ("\nSorry, cannot add Event. The Event you have entered overlaps with an existing event.\n")
-           where result = (listOfEvents x es)
+-- | write the new Event to the Events file if it does not overlap with existing
+-- event returns the list of events with the new event at the head on success,
+-- or the existing list on failure
+addEvent :: Event -> [Event] -> IO [Event]
+addEvent e es = either return save $ addEventToList e es
+  where save xs = do
+          encodeFile eventFile xs
+          return xs
+
+
+addEventToList :: Event -> [Event] -> Either [Event] [Event]
+addEventToList x es = if existingEvent x es then Left es else Right (x:es)
 
 
 -- adds the new event to the list of events if it does not overlap with any existing events
@@ -89,13 +92,13 @@ existingEvent x (e:es) =  if (startTime x) < (endTime e) && (startTime e) < (end
 
 
 sortEvents :: [Event] -> [Event]
-sortEvents es =  sortBy (comparing startTime) es
+sortEvents es = sortBy (comparing startTime) es
 
 
 formatEvent :: TimeZone -> Event -> String
 formatEvent tz e = "\n" ++ name e ++ "\n  " ++ "Starts At: "
                    ++ formatTime defaultTimeLocale "%c"  (utcToLocalTime tz (startTime e)) ++ "\n  "
-                   ++ "End Time: " ++ formatTime defaultTimeLocale "%c"  (utcToLocalTime tz (endTime e)) ++ "\n  " 
+                   ++ "End Time: " ++ formatTime defaultTimeLocale "%c"  (utcToLocalTime tz (endTime e)) ++ "\n  "
                    ++  detail e ++ "\n" ++ "-------------------------" ++ "\n"
 
 eventsToString :: TimeZone -> [Event] -> String
@@ -111,15 +114,3 @@ displayEvents = do es <- getCurrentEvents
 {- read in minutes, or hours  then convert to NominalDiffTime
 then iterate through list and if the element startTime - current Time (diffUtcTime) is
 equal to the nominal diff time then put in seperate list and display list -}
-
-
-
-
-
-
-
-
-
-
-
-
